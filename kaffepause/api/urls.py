73e6fba014a4bb5ced5f7a9a861736a1c62a901e
Eirik Steira <eirsteir@stud.ntnu.no>
2020-11-04 20:03:27 +0100

    Fix base redirect to api version

--- kaffepause/api/urls.py
+++ kaffepause/api/urls.py
@@ -1,5 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url
-from django.urls import include, re_path, reverse_lazy
+from django.urls import include
 from django.views.generic import RedirectView
 
 from kaffepause.api.v1 import router as v1
@@ -7,7 +8,7 @@ from kaffepause.api.v1 import router as v1
 app_name = "api"
 urlpatterns = [
     url(r"^v1/", include((v1.urls, "v1"), namespace="v1")),
-    # the 'api-root' from django rest-frameworks default router
-    # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
-    re_path(r"^$", RedirectView.as_view(url=reverse_lazy("api-root"), permanent=False)),
+    url(
+        r"^$", RedirectView.as_view(url=f"/api/{settings.API_VERSION}/"), name="default"
+    ),
 ]
