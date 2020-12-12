f22a2c8d29f5a29580c631535c10a3a9fcc4c5ea
Eirik Steira <eirsteir@stud.ntnu.no>
2020-12-12 16:19:05 +0100

    Add snapshot testing

--- kaffepause/statusupdates/test/test_mutations.py
+++ kaffepause/statusupdates/test/test_mutations.py
@@ -1,11 +1,13 @@
 import json
 
 import pytest
+from graphene.test import Client
 from graphene_django.utils.testing import graphql_query
 from graphql_jwt.settings import jwt_settings
 from graphql_jwt.shortcuts import get_token
 from graphql_jwt.testcases import JSONWebTokenTestCase
 
+from kaffepause.api.schema import schema
 from kaffepause.statusupdates.enums import StatusUpdateType
 from kaffepause.statusupdates.test.graphql_requests import UPDATE_STATUS_MUTATION
 
@@ -41,16 +43,28 @@ def test_update_status_updates_users_current_status(client_query, token, user):
 
 def test_update_status_returns_current_status(client_query, token, user):
     """Should return the newly updated status."""
+    expected_status_type = StatusUpdateType.FOCUSMODE
+    response = client_query(
+        UPDATE_STATUS_MUTATION,
+        op_name="updateStatus",
+        variables={"statusType": expected_status_type.name},
+        headers={jwt_settings.JWT_AUTH_HEADER_NAME: token},
+    )
+    content = response.json()
+    actual_current_status = content.get("data").get("updateStatus").get("currentStatus")
+
+    assert actual_current_status.get("statusType") == expected_status_type.name
+    assert actual_current_status.get("verb") == expected_status_type.value
+
+
+def test_update_status_unauthenticated(snapshot, client_query):
+
     expected_status_type = StatusUpdateType.FOCUSMODE.name
     response = client_query(
         UPDATE_STATUS_MUTATION,
         op_name="updateStatus",
         variables={"statusType": expected_status_type},
-        headers={jwt_settings.JWT_AUTH_HEADER_NAME: token},
     )
     content = response.json()
-    print(content)
-    assert (
-        content.get("data").get("updateStatus").get("currentStatus").get("statusType")
-        == expected_status_type
-    )
+
+    snapshot.assert_match(content)
