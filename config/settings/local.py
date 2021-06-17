45656a1f2e8d57e2621a158f54856198cd5e978b
Eirik Steira <eirsteir@stud.ntnu.no>
2021-06-17 16:58:02 +0200

    fix(settings): set allowed hosts to all

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="7oJzsiMQ07gMRJnubSXQNT9vydGVmB4z9mQqwfe3cfTr63xXtPCtddgRKgf6kCBu",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
