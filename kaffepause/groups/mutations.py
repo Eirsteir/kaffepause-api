d1b8a3febdb1c49552ee8d6f182c495b1353958b
Eirik Steira <steiraeirik@gmail.com>
2023-05-03 17:54:53 +0200

    Create group with initial members mutation

--- kaffepause/groups/mutations.py
+++ kaffepause/groups/mutations.py
@@ -10,12 +10,13 @@ class CreateGroup(
 ):
     class Arguments:
         name = graphene.String(required=True)
+        members = graphene.List(graphene.UUID, required=False)
 
     group = graphene.Field(GroupNode)
 
     @classmethod
-    def resolve_mutation(cls, root, info, name):
+    def resolve_mutation(cls, root, info, name, members=None):
         current_user = cls.get_current_user()
-        group = create_group(actor=current_user, name=name)
+        group = create_group(actor=current_user, name=name, members=members)
         return cls(group=group, success=True)
 
--- kaffepause/groups/services.py
+++ kaffepause/groups/services.py
@@ -1,11 +1,24 @@
+from typing import List
+from uuid import UUID
+
 from kaffepause.groups.models import Group
 from kaffepause.users.models import User
 
 
-def create_group(*, actor: User, name: str) -> Group:
+def create_group(*, actor: User, name: str, members: List[UUID] = None) -> Group:
+
     group = Group(name=name).save()
     group.creator.connect(actor)
     group.members.connect(actor)
 
+    if members:
+        add_members_to_group(actor, group, members)
+
     return group
+
+
+def add_members_to_group(actor, group, members):
+    members = actor.friends.filter(uuid__in=members)
+    for member in members:
+        group.members.connect(member)
 
